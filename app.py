98891ca1d980f438888bfab490b896f8bf9bf01f
fviniciussilva <fv050795@gmail.com>
2026-08-07 14:59:21 -0300

    Adiciona carrinho dinamico aos produtos

--- app.py
+++ app.py
@@ -11,7 +11,20 @@ produtos = [
 
 
 def main(page: ft.Page):
+
     page.title = "Açaí Flow"
+
+    texto_carrinho = ft.Text("Carrinho vazio")
+
+
+    def adicionar_ao_carrinho(produto):
+
+        carrinho.append(produto)
+
+        texto_carrinho.value = f"Itens no carrinho: {len(carrinho)}"
+
+        page.update()
+
 
     page.add(
         ft.Text(
@@ -21,11 +34,20 @@ def main(page: ft.Page):
         )
     )
 
+
+    page.add(texto_carrinho)
+
+
     for produto in produtos:
+
         page.add(
             ft.Text(produto),
+
             ft.ElevatedButton(
                 "Adicionar",
-                on_click=lambda e: carrinho.append(produto)
+                on_click=lambda e, produto=produto: adicionar_ao_carrinho(produto)
             )
         )
+
+
+ft.run(main)
